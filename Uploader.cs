e4c39e7b6dde63d0695fded5caebaeefdbe3f7b1
Louis Matthijssen <louis@ict-crew.nl>
2014-08-17 10:06:34 +0200

    FTP and CIFS without SSH.NET

--- Uploader.cs
+++ Uploader.cs
@@ -92,7 +92,7 @@ namespace NFU
         }
 
         /// <summary>
-        /// The BackgroundWorker for the upload
+        /// The BackgroundWorker for the upload.
         /// </summary>
         static void UploadWorkerHandler(object sender, DoWorkEventArgs a)
         {
@@ -105,127 +105,29 @@ namespace NFU
 
                 switch (Settings.Default.TransferType)
                 {
-
                     case 0:
                     case 3:
-                        try
-                        {
-                            byte[] Buffer = new byte[1024 * 10];
-
-                            FtpWebRequest FTPrequest;
-
-                            if (!String.IsNullOrEmpty(Settings.Default.Directory))
-                            {
-                                FTPrequest = (FtpWebRequest)WebRequest.Create(String.Format("ftp://{0}:{1}/{2}/{3}", Settings.Default.Host, Settings.Default.Port, Settings.Default.Directory, filename));
-                            }
-                            else
-                            {
-                                FTPrequest = (FtpWebRequest)WebRequest.Create(String.Format("ftp://{0}:{1}/{2}", Settings.Default.Host, Settings.Default.Port, filename));
-                            }
-
-                            if (Settings.Default.TransferType == 3)
-                            {
-                                FTPrequest.EnableSsl = true;
-                                ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(Misc.ValidateServerCertificate);
-                            }
-
-                            FTPrequest.KeepAlive = false;
-                            FTPrequest.Method = WebRequestMethods.Ftp.UploadFile;
-
-                            FTPrequest.Credentials = new NetworkCredential(Settings.Default.Username, Misc.Decrypt(Settings.Default.Password));
-
-                            using (FileStream inputStream = File.OpenRead(path))
-                            using (Stream outputStream = FTPrequest.GetRequestStream())
-                            {
-                                long totalReadBytesCount = 0;
-                                int readBytesCount;
-                                while ((readBytesCount = inputStream.Read(Buffer, 0, Buffer.Length)) > 0)
-                                {
-                                    if (uploadWorker.CancellationPending) return;
-
-                                    outputStream.Write(Buffer, 0, readBytesCount);
-                                    totalReadBytesCount += readBytesCount;
-
-                                    uploadWorker.ReportProgress((int)(totalReadBytesCount * 100 / inputStream.Length));
-                                }
-                            }
-                        }
-                        catch (Exception e)
-                        {
-                            abort = true;
-                            uploadSuccess = false;
-                            Misc.HandleError(e, "FTP(S)");
-                        }
-
+                        abort = uploadFTP(path, filename);
                         break;
 
                     case 1:
                         try
                         {
-                            using (FileStream inputStream = new FileStream(path, FileMode.Open))
-                            using (SftpClient outputStream = new SftpClient(Settings.Default.Host, Settings.Default.Port, Settings.Default.Username, Misc.Decrypt(Settings.Default.Password)))
-                            {
-                                outputStream.Connect();
-
-                                if (!String.IsNullOrEmpty(Settings.Default.Directory)) outputStream.ChangeDirectory(Settings.Default.Directory);
-
-                                IAsyncResult Async = outputStream.BeginUploadFile(inputStream, filename);
-                                SftpUploadAsyncResult SFTPAsync = Async as SftpUploadAsyncResult;
-
-                                while (!SFTPAsync.IsCompleted)
-                                {
-                                    uploadWorker.ReportProgress((int)(SFTPAsync.UploadedBytes * 100 / (ulong)inputStream.Length));
-                                }
-
-                                outputStream.EndUploadFile(Async);
-                            }
+                            abort = uploadSFTP(path, filename);
                         }
-                        catch (Exception e)
+                        catch (Exception err)
                         {
-                            abort = true;
-                            uploadSuccess = false;
-                            Misc.HandleError(e, "SFTP");
-                        }
+                            MessageBox.Show("NFU relies on the SSH.NET library for SFTP uploads.\nPlease download the DLL in the same directory as the NFU executable.\nThe download link can be found in the about box.",
+                                "Missing DLL", MessageBoxButtons.OK, MessageBoxIcon.Error);
 
+                            uploadSuccess = false;
+                            Misc.HandleError(err, "SFTP");
+                            abort = true;
+                        }
                         break;
 
                     case 2:
-                        try
-                        {
-                            byte[] Buffer = new byte[1024 * 10];
-
-                            IntPtr Token = IntPtr.Zero;
-                            Misc.LogonUser(Settings.Default.Username, "NFU", Misc.Decrypt(Settings.Default.Password), 9, 0, ref Token);
-                            WindowsIdentity Identity = new WindowsIdentity(Token);
-
-                            string DestPath = (Settings.Default.Directory != null) ? String.Format(@"\\{0}\{1}\{2}", Settings.Default.Host, Settings.Default.Directory, filename) : String.Format(@"\\{0}\{1}", Settings.Default.Host, filename);
-
-                            using (Identity.Impersonate())
-                            using (FileStream inputStream = new FileStream(path, FileMode.Open, FileAccess.Read))
-                            using (FileStream outputStream = new FileStream(DestPath, FileMode.CreateNew, FileAccess.Write))
-                            {
-                                long FileLength = inputStream.Length;
-                                long TotalBytes = 0;
-                                int CurrentBlockSize;
-
-                                while ((CurrentBlockSize = inputStream.Read(Buffer, 0, Buffer.Length)) > 0)
-                                {
-                                    if (uploadWorker.CancellationPending) return;
-
-                                    outputStream.Write(Buffer, 0, CurrentBlockSize);
-                                    TotalBytes += CurrentBlockSize;
-
-                                    uploadWorker.ReportProgress((int)(TotalBytes * 100 / FileLength));
-                                }
-                            }
-                        }
-                        catch (Exception e)
-                        {
-                            abort = true;
-                            uploadSuccess = false;
-                            Misc.HandleError(e, "CIFS");
-                        }
-
+                        abort = uploadCIFS(path, filename);
                         break;
                 }
 
@@ -233,6 +135,154 @@ namespace NFU
                     break;
 
                 currentIndex++;
+            }
+        }
+
+        /// <summary>
+        /// Upload a file via FTP(s).
+        /// </summary>
+        /// <param name="path">The path of the local file.</param>
+        /// <param name="filename">The filename of the remote file.</param>
+        /// <returns>True on failure, false on success.</returns>
+        static bool uploadFTP(string path, string filename)
+        {
+            try
+            {
+                byte[] Buffer = new byte[1024 * 10];
+
+                FtpWebRequest FTPrequest;
+
+                if (!String.IsNullOrEmpty(Settings.Default.Directory))
+                {
+                    FTPrequest = (FtpWebRequest)WebRequest.Create(String.Format("ftp://{0}:{1}/{2}/{3}", Settings.Default.Host, Settings.Default.Port, Settings.Default.Directory, filename));
+                }
+                else
+                {
+                    FTPrequest = (FtpWebRequest)WebRequest.Create(String.Format("ftp://{0}:{1}/{2}", Settings.Default.Host, Settings.Default.Port, filename));
+                }
+
+                if (Settings.Default.TransferType == 3)
+                {
+                    FTPrequest.EnableSsl = true;
+                    ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(Misc.ValidateServerCertificate);
+                }
+
+                FTPrequest.KeepAlive = false;
+                FTPrequest.Method = WebRequestMethods.Ftp.UploadFile;
+
+                FTPrequest.Credentials = new NetworkCredential(Settings.Default.Username, Misc.Decrypt(Settings.Default.Password));
+
+                using (FileStream inputStream = File.OpenRead(path))
+                using (Stream outputStream = FTPrequest.GetRequestStream())
+                {
+                    long totalReadBytesCount = 0;
+                    int readBytesCount;
+                    while ((readBytesCount = inputStream.Read(Buffer, 0, Buffer.Length)) > 0)
+                    {
+                        if (uploadWorker.CancellationPending) return true;
+
+                        outputStream.Write(Buffer, 0, readBytesCount);
+                        totalReadBytesCount += readBytesCount;
+
+                        uploadWorker.ReportProgress((int)(totalReadBytesCount * 100 / inputStream.Length));
+                    }
+                }
+
+                return false;
+            }
+            catch (Exception e)
+            {
+                uploadSuccess = false;
+                Misc.HandleError(e, "FTP(S)");
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Upload a file via SFTP.
+        /// </summary>
+        /// <param name="path">The path of the local file.</param>
+        /// <param name="filename">The filename of the remote file.</param>
+        /// <returns>True on failure, false on success.</returns>
+        static bool uploadSFTP(string path, string filename)
+        {
+            try
+            {
+                using (FileStream inputStream = new FileStream(path, FileMode.Open))
+                using (SftpClient outputStream = new SftpClient(Settings.Default.Host, Settings.Default.Port, Settings.Default.Username, Misc.Decrypt(Settings.Default.Password)))
+                {
+                    outputStream.Connect();
+
+                    if (!String.IsNullOrEmpty(Settings.Default.Directory)) outputStream.ChangeDirectory(Settings.Default.Directory);
+
+                    IAsyncResult Async = outputStream.BeginUploadFile(inputStream, filename);
+                    SftpUploadAsyncResult SFTPAsync = Async as SftpUploadAsyncResult;
+
+                    while (!SFTPAsync.IsCompleted)
+                    {
+                        if (uploadWorker.CancellationPending) return true;
+
+                        uploadWorker.ReportProgress((int)(SFTPAsync.UploadedBytes * 100 / (ulong)inputStream.Length));
+                    }
+
+                    outputStream.EndUploadFile(Async);
+                }
+
+                return false;
+            }
+            catch (Exception e)
+            {
+                uploadSuccess = false;
+                Misc.HandleError(e, "SFTP");
+                return true;
+            }
+        }
+
+
+        /// <summary>
+        /// Upload a file via CIFS.
+        /// </summary>
+        /// <param name="path">The path of the local file.</param>
+        /// <param name="filename">The filename of the remote file.</param>
+        /// <returns>True on failure, false on success.</returns>
+        static bool uploadCIFS(string path, string filename)
+        {
+            try
+            {
+                byte[] Buffer = new byte[1024 * 10];
+
+                IntPtr Token = IntPtr.Zero;
+                Misc.LogonUser(Settings.Default.Username, "NFU", Misc.Decrypt(Settings.Default.Password), 9, 0, ref Token);
+                WindowsIdentity Identity = new WindowsIdentity(Token);
+
+                string DestPath = (Settings.Default.Directory != null) ? String.Format(@"\\{0}\{1}\{2}", Settings.Default.Host, Settings.Default.Directory, filename) : String.Format(@"\\{0}\{1}", Settings.Default.Host, filename);
+
+                using (Identity.Impersonate())
+                using (FileStream inputStream = new FileStream(path, FileMode.Open, FileAccess.Read))
+                using (FileStream outputStream = new FileStream(DestPath, FileMode.CreateNew, FileAccess.Write))
+                {
+                    long FileLength = inputStream.Length;
+                    long TotalBytes = 0;
+                    int CurrentBlockSize;
+
+                    while ((CurrentBlockSize = inputStream.Read(Buffer, 0, Buffer.Length)) > 0)
+                    {
+                        if (uploadWorker.CancellationPending) return true;
+
+                        outputStream.Write(Buffer, 0, CurrentBlockSize);
+                        TotalBytes += CurrentBlockSize;
+
+                        uploadWorker.ReportProgress((int)(TotalBytes * 100 / FileLength));
+                    }
+                }
+
+                return false;
+            }
+            catch (Exception e)
+            {
+                uploadSuccess = false;
+                Misc.HandleError(e, "CIFS");
+                return true;
             }
         }
 
