ba0c278e6c8afba8297c7f9fc585b7d2ecdcc0f6
Louis Matthijssen <louis@ict-crew.nl>
2014-12-27 20:01:28 +0100

    Add unit tests for future use

---
+++ Tests/FileTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using NFU.Models;
+using System.IO;
+
+namespace Tests
+{
+    [TestClass]
+    public class FileTests
+    {
+        [TestMethod]
+        public void AfterUploadTest()
+        {
+            var files = new List<UploadFile> {
+                new UploadFile(FileState.Temporary),
+                new UploadFile(FileState.Temporary)
+            };
+
+            // Set the state of the second file to normal
+            // There's one temporary and one normal file now
+            files[1].State = FileState.Normal;
+
+            foreach (var file in files)
+            {
+                file.AfterUpload();
+
+                if (file.State == FileState.Normal && !File.Exists(file.Path))
+                {
+                    Assert.Fail("Normal file has been deleted.");
+                }
+                if (file.State == FileState.Temporary && File.Exists(file.Path))
+                {
+                    Assert.Fail("Temporary fail has not been deleted.");
+                }
+            }
+        }
+    }
+}
